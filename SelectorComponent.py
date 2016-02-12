152bc27f6daf25f1d06e2fadd64e94aaef203648
matth <matthewberryhill@gmail.com>
2016-02-12 00:04:09 -0700

    Selector Component, Matrix, Session Component set up and working

---
+++ SelectorComponent.py
@@ -0,0 +1,22 @@
+from _Framework.SessionComponent import SessionComponent
+from _Framework.ModeSelectorComponent import ModeSelectorComponent
+
+
+class SelectorComponent(ModeSelectorComponent):
+	
+	def __init__(self, matrix, shift_button):
+		ModeSelectorComponent.__init__(self)
+		self._session = SessionComponent(matrix.width(), matrix.height())
+		self._matrix = matrix
+		self._shift_button = shift_button
+		self._setup_session()
+
+	def get_session_component(self):
+		return self._session
+
+	def _setup_session(self):
+		for scene_index in range(1):
+			scene = self._session.scene(scene_index)
+			for track_index in range(4):
+				button = self._matrix.get_button(track_index, scene_index)
+				scene.clip_slot(track_index).set_launch_button(button)
